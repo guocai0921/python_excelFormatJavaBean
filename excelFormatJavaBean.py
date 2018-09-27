7aa8b9e5077b1283e372ab7bbd4ca9e102833ed9
guocai0921 <923055433@qq.com>
2018-09-27 16:56:58 +0800

    v1.1

--- excelFormatJavaBean.py
+++ excelFormatJavaBean.py
@@ -34,50 +34,52 @@ try:
 except:
     print("fail to open file")
 else:
-    # 读取第一个sheet
-    table = data.sheets()[0]
-    # 获取行数
-    n = table.nrows
-    name = table.name
-    contents = []
-    for i in range(n):
-        data = {}
-        # 取第i行第0列的值
-        code = table.cell(i, 0).value
-        content = table.cell(i, 1).value
-        types = table.cell(i, 2).value
-        lengths = table.cell(i, 3).value
-        # annotation = table.cell(i, 4).value
-        data["name"] = code
-        data["content"] = content
-        code = convert(code, '_')
-        data["code"] = code
-        # data["annotation"] = int(math.floor(annotation))
-        data["uname"] = formatToStr(code)
-        if types == 'C':
-            data["types"] = 'VARCHAR2'
-            data["lengths"] = int(math.floor(lengths))
-            data["mold"] = 'String'
-        elif types == 'N':
-            data["types"] = 'NUMBER'
-            if isinstance(lengths, float):
-                if lengths == int(lengths):  # checking for the integer:
-                    data["lengths"] = int(lengths)  # solving your problem and printing the integer
+    print(data.sheet_names())
+    workbook = data.sheet_names()
+    for table in data.sheets():
+        # 获取行数
+        n = table.nrows
+        name = table.name
+        fileName = name[1:]
+        contents = []
+        for i in range(n):
+            data = {}
+            # 取第i行第0列的值
+            code = table.cell(i, 0).value
+            content = table.cell(i, 1).value
+            types = table.cell(i, 2).value
+            lengths = table.cell(i, 3).value
+            # annotation = table.cell(i, 4).value
+            data["name"] = code
+            data["content"] = content
+            code = convert(code, '_')
+            data["code"] = code
+            # data["annotation"] = int(math.floor(annotation))
+            data["uname"] = formatToStr(code)
+            if types == 'C':
+                data["types"] = 'VARCHAR2'
+                data["lengths"] = int(math.floor(lengths))
+                data["mold"] = 'String'
+            elif types == 'N':
+                data["types"] = 'NUMBER'
+                if isinstance(lengths, float):
+                    if lengths == int(lengths):  # checking for the integer:
+                        data["lengths"] = int(lengths)  # solving your problem and printing the integer
+                    else:
+                        data["lengths"] = lengths
+                    data["mold"] = 'Integer'
                 else:
                     data["lengths"] = lengths
-                data["mold"] = 'Integer'
-            else:
-                data["lengths"] = lengths
-                data["mold"] = 'Double'
-        contents.append(data)
-    print(contents)
-    # with open("E:\\formatExcel\\template\\template.json.j4", "r", encoding='UTF-8') as fd:
-    with open("E:\\formatExcel\\template\\template.json.j3", "r", encoding='UTF-8') as fd:
-    # with open("E:\\formatExcel\\template\\template.json.j1", "r", encoding='UTF-8') as fd:
-        template = Template(fd.read())
-    config_content = template.render(contents=contents, name=name)
+                    data["mold"] = 'Double'
+            contents.append(data)
+        print(contents)
+        # with open("E:\\formatExcel\\template\\template.json.j4", "r", encoding='UTF-8') as fd:
+        # with open("E:\\formatExcel\\template\\template.json.j3", "r", encoding='UTF-8') as fd:
+        with open("E:\\formatExcel\\template\\template.json.j1", "r", encoding='UTF-8') as fd:
+            template = Template(fd.read())
+        config_content = template.render(contents=contents, name=name, fileName=fileName)
 
-    # with open("E:\\formatExcel\\productFiles\\" + name + ".sql", "w") as fd:
-    with open("E:\\formatExcel\\productFiles\\" + name + ".txt", "w") as fd:
-    # with open("E:\\formatExcel\\productFiles\\" + name + ".java", "w") as fd:
-        fd.write(config_content)
+        # with open("E:\\formatExcel\\productFiles\\" + name + ".sql", "w") as fd:
+        # with open("E:\\formatExcel\\productFiles\\" + name + ".txt", "w") as fd:
+        with open("E:\\formatExcel\\productFiles\\" + name + ".java", "w") as fd:
+            fd.write(config_content)
